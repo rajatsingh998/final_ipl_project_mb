a510b266d23d9f40d96e9109f60af6131e82d0bf
rajat singh <rajatsingh997.rs@gmail.com>
2019-11-22 08:56:11 +0530

    V3

--- my_project.java
+++ my_project.java
@@ -16,39 +16,59 @@ import com.opencsv.*;
 import java.io.IOException;
 
 import static java.util.Collections.*;
+public class my_project {
+    public static void main(String[] args) {
 
- class Calculation{
-     public  static  HashMap<String, Integer> allSolution(int n){
-         String matches="matches.csv"; //Give dataset path here
-         String deliveries="deliveries.csv";
-         ArrayList<HashMap<String, String>> data;
-         ArrayList<HashMap<String, String>> data1;
-         data=add(matches);
-         data1=add(deliveries);
-         HashMap<String, Integer> ok= new HashMap<>();
-         switch(n){
-             case 1:
-                 HashMap<String, Integer> noOfMatchesPlayed= noOfMatchesPlayedFun(data);
-                 return noOfMatchesPlayed;
-             case 2:
-                 HashMap<String, Integer> noOfMatchesWon= noOfMatchesWonFun(data);
-                 return noOfMatchesWon;
-             case 3:
-                 HashMap<String, Integer> extraRuns= extraRunsFun(data,data1);
-                 return extraRuns;
-             case 4:
-                 HashMap<String, Integer> topEconomicalBowler= topEconomicalBowlerFun(data,data1);
-                 return topEconomicalBowler;
-             case 5:
-                 HashMap<String, Integer> noOfPlayerOfTheMatch= noOfPlayerOfTheMatchFun(data);
-                 return noOfPlayerOfTheMatch;
-             default:
-                 return ok;
-         }
+        System.out.println("====== IPL DATASET PROJECT =====");
+        System.out.println("\n Choose one of the option");
+        System.out.println("\n 1. Number of matches played per year of all the years in IPL.");
+        System.out.println("\n 2. Number of matches won of all teams over all the years of IPL.");
+        System.out.println("\n 3. For the year 2016 get the extra runs conceded per team.");
+        System.out.println("\n 4. For the year 2015 get the top economical bowlers.");
+        System.out.println("\n 5. Total number of the player of the match award by players");
 
-     }
+        Scanner sc = new Scanner(System.in);
+        int response = sc.nextInt();
+        getSolution object = new getSolution();
+        HashMap<String, Integer> solution = getSolution.allSolution(response);
+        solution.forEach((k, v) -> System.out.printf("\n %-30s : %d", k, v));
 
-     private   static HashMap<String,Integer> noOfMatchesPlayedFun(ArrayList<HashMap<String, String>>data) {
+    }
+}
+
+class getSolution {
+
+    private static storingMatchData storingMatchDataObject= new storingMatchData();
+    private static ArrayList<HashMap<String, String>> data=storingMatchDataObject.dataParsing("matches.csv");;
+    private  static ArrayList<HashMap<String, String>> data1= storingMatchDataObject.dataParsing("deliveries.csv");
+
+    public  static  HashMap<String, Integer> allSolution(int response){
+        HashMap<String, Integer> ok= new HashMap<>();
+        switch(response){
+            case 1:
+                noOfMatchesPlayedClass noOfMatchesPlayedObject= new noOfMatchesPlayedClass();
+                return noOfMatchesPlayedObject.noOfMatchesPlayedFun(data);
+            case 2:
+                noOfMatchesWonClass noOfMatchesWonObject= new noOfMatchesWonClass();
+                return noOfMatchesWonObject.noOfMatchesWonFun(data);
+            case 3:
+                extraRunsClass extraRunsObject= new extraRunsClass();
+                return extraRunsObject.extraRunsFun(data,data1);
+            case 4:
+                topEconomicalBowlerClass topEconomicalBowlerObject= new topEconomicalBowlerClass();
+                return topEconomicalBowlerObject.topEconomicalBowlerFun(data,data1);
+            case 5:
+                noOfPlayerOfTheMatchClass noOfPlayerOfTheMatchObject= new noOfPlayerOfTheMatchClass();
+                return noOfPlayerOfTheMatchObject.noOfPlayerOfTheMatchFun(data);
+            default:
+                return ok;
+        }
+
+    }
+}
+
+class noOfMatchesPlayedClass{
+    public  HashMap<String,Integer> noOfMatchesPlayedFun(ArrayList<HashMap<String, String>>data) {
          HashMap<String, Integer> map= new HashMap<>();
          String date;
          for(HashMap<String, String> i :data){
@@ -64,8 +84,10 @@ import static java.util.Collections.*;
          }
          return  map;
      }
+ }
 
-     private static HashMap<String, Integer> noOfMatchesWonFun(ArrayList<HashMap<String, String>>data){
+ class noOfMatchesWonClass{
+     public  HashMap<String, Integer> noOfMatchesWonFun(ArrayList<HashMap<String, String>>data){
          String team;
          HashMap<String, Integer> map= new HashMap<>();
          for(HashMap<String, String> i :data){
@@ -82,9 +104,11 @@ import static java.util.Collections.*;
          }
          return map;
      }
+ }
 
-     private  static  HashMap<String,Integer> extraRunsFun(ArrayList<HashMap<String, String>>data,
-                                                          ArrayList<HashMap<String, String>>data1){
+ class extraRunsClass{
+     public  HashMap<String,Integer> extraRunsFun(ArrayList<HashMap<String, String>>data,
+                                                           ArrayList<HashMap<String, String>>data1){
          ArrayList<String> Id= new ArrayList<>();
          String year;
          for(HashMap<String, String> i :data){
@@ -115,8 +139,11 @@ import static java.util.Collections.*;
          }
          return map;
      }
-     private  static  HashMap<String,Integer> topEconomicalBowlerFun(ArrayList<HashMap<String, String>>data,
-                                                                    ArrayList<HashMap<String, String>>data1) {
+ }
+
+ class topEconomicalBowlerClass{
+     public HashMap<String,Integer> topEconomicalBowlerFun(ArrayList<HashMap<String, String>>data,
+                                                                     ArrayList<HashMap<String, String>>data1) {
          ArrayList<String> id= new ArrayList<>();
          for(HashMap<String, String> i :data){
              String year=i.get("season");
@@ -126,7 +153,7 @@ import static java.util.Collections.*;
          }
 
          HashMap<String, Integer> player= new HashMap<>();
-         HashMap<String, Integer> ques4= new HashMap<>();
+         HashMap<String, Integer> temp= new HashMap<>();
          HashMap<String, Integer> bowl= new HashMap<>();
          for(HashMap<String, String> i :data1){
              String val=i.get("match_id");
@@ -154,17 +181,18 @@ import static java.util.Collections.*;
              }
          }
 
-         for(String keyy: player.keySet()){
-             String playerName=keyy;
+         for(String key: player.keySet()){
+             String playerName=key;
              int totalBall=bowl.get(playerName);
              int over=(totalBall/6);
              int totalRuns= player.get(playerName);
 
              int eco=(totalRuns/over);
-             ques4.put(playerName, eco);
+             temp.put(playerName, eco);
 
          }
-         HashMap<String, Integer> finals=sortByValue(ques4);
+         sortByValueClass sortByValueObject= new sortByValueClass();
+         HashMap<String, Integer> finals=sortByValueObject.sortByValueFun(temp);
          HashMap<String, Integer> ans= new HashMap<>();
          int i=1;
          for (Map.Entry<String, Integer> entry : finals.entrySet())
@@ -175,10 +203,13 @@ import static java.util.Collections.*;
                  break;
              }
          }
+
          return ans;
      }
+ }
 
-     private static HashMap<String, Integer> noOfPlayerOfTheMatchFun(ArrayList<HashMap<String, String>>data){
+ class noOfPlayerOfTheMatchClass{
+     public HashMap<String, Integer> noOfPlayerOfTheMatchFun(ArrayList<HashMap<String, String>>data){
          HashMap<String, Integer> map= new HashMap<>();
          String playerOfTheMatch;
          for(HashMap<String, String> i :data){
@@ -196,70 +227,60 @@ import static java.util.Collections.*;
          }
          return map;
      }
-
-     private static  ArrayList<HashMap<String, String>> add(String my_file) {
-         try {
-             FileReader filereader = new FileReader(my_file);
-             CSVReader csvReader = new CSVReaderBuilder(filereader).withSkipLines(0).build();
-             List<String[]> allData = csvReader.readAll();
-             ArrayList<HashMap<String, String>> data = new ArrayList<HashMap<String, String>>();
-
-
-             String[] keys = allData.get(0);
-             for (String[] row : allData) {
-                 int counter = 0;
-                 HashMap<String, String> map = new HashMap<String, String>();
-                 for (String cell : row) {
-                     map.put(keys[counter], cell);
-                     counter++;
-                 }
-                 data.add(map);
-
-             }
-             data.remove(0);
-             return data;
-         } catch (Exception e) {
-             e.printStackTrace();
-         }
-         ArrayList<HashMap<String, String>> data = new ArrayList<HashMap<String, String>>();
-         return data;}
-
-     private static HashMap<String, Integer> sortByValue(HashMap<String, Integer> ques4)
-     {
-
-         List<Map.Entry<String, Integer> > list = new LinkedList<>(ques4.entrySet());
-
-
-         Collections.sort(list, Comparator.comparing(Map.Entry::getValue));
-
-
-         HashMap<String, Integer> temp = new LinkedHashMap<>();
-         for (Map.Entry<String, Integer> tempp : list) {
-             temp.put(tempp.getKey(), tempp.getValue());
-         }
-         return temp;
-     }
-
  }
 
+ class storingMatchData{
 
-public class my_project
-{
-    public static void main(String[] args) {
+    ArrayList<HashMap<String, String>> data;
+    public ArrayList<HashMap<String, String>> dataParsing(String my_file) {
+        try {
+            FileReader filereader = new FileReader(my_file);
+            CSVReader csvReader = new CSVReaderBuilder(filereader).withSkipLines(0).build();
+            List<String[]> allData = csvReader.readAll();
+            ArrayList<HashMap<String, String>> data = new ArrayList<HashMap<String, String>>();
 
-            System.out.println("====== IPL DATASET PROJECT =====");
-            System.out.println("\n Choose one of the option");
-            System.out.println("\n 1. Number of matches played per year of all the years in IPL.");
-            System.out.println("\n 2. Number of matches won of all teams over all the years of IPL.");
-            System.out.println("\n 3. For the year 2016 get the extra runs conceded per team.");
-            System.out.println("\n 4. For the year 2015 get the top economical bowlers.");
-            System.out.println("\n 5. Total number of the player of the match award by players");
 
-            Scanner sc=new Scanner(System.in);
-            int response= sc.nextInt();
-            Calculation object= new Calculation();
-            HashMap<String, Integer>solution= object.allSolution(response);
-            solution.forEach((k,v)->System.out.printf("\n %-30s : %d",k,v));
+            String[] keys = allData.get(0);
+            for (String[] row : allData) {
+                int counter = 0;
+                HashMap<String, String> map = new HashMap<String, String>();
+                for (String cell : row) {
+                    map.put(keys[counter], cell);
+                    counter++;
+                }
+                data.add(map);
 
+            }
+            data.remove(0);
+            return data;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        ArrayList<HashMap<String, String>> data = new ArrayList<HashMap<String, String>>();
+        return data;}
+
+}
+
+class sortByValueClass {
+    public HashMap<String, Integer> sortByValueFun(HashMap<String, Integer> temp) {
+        List<Map.Entry<String, Integer>> list = new LinkedList<>(temp.entrySet());
+        Collections.sort(list, Comparator.comparing(Map.Entry::getValue));
+
+        HashMap<String, Integer> map = new LinkedHashMap<>();
+        for (Map.Entry<String, Integer> tempp : list) {
+            map.put(tempp.getKey(), tempp.getValue());
+        }
+        return map;
     }
 }
+
+
+
+
+
+
+
+
+
+
+
